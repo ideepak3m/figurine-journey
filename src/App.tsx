c50684371bc836e9d8d80dc64981a6c96fe83634
ideepak3m <ivdeepak@gmail.com>
2026-04-02 11:09:58 -0400

    Switch from HashRouter to BrowserRouter for clean URLs (SEO friendly)

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 import Index from "./pages/Index";
 import { Navigate } from "react-router-dom";
@@ -30,7 +30,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <HashRouter>
+        <BrowserRouter>
           <Routes>
             <Route path="/" element={<Navigate to="/shop" replace />} />
             <Route path="/shop" element={<Shop />} />
@@ -48,7 +48,7 @@ const App = () => (
             <Route path="/login" element={<Login />} />
             <Route path="/reset-password" element={<ResetPassword />} />
           </Routes>
-        </HashRouter>
+        </BrowserRouter>
         <Analytics />
       </TooltipProvider>
     </QueryClientProvider>
